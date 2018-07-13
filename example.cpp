72ca7a2477f6ceea1da847febb5bbcd2a31d5b1b
Kevin Server <kevinserver@yahoo.com>
2018-07-12 19:29:49 -0700

    Made empty functions
    this is my comment

---
+++ example.cpp
@@ -0,0 +1,12 @@
+#include "example.h"
+
+
+void foo()
+{
+
+}
+
+int bar(char mutatee)
+{
+	return 0;
+}
---
+++ example.h
@@ -0,0 +1,9 @@
+#ifndef EXAMPLE_H_
+#define EXAMPLE_H_
+
+void foo();
+
+int bar(char mutatee);
+
+
+#endif /*EXAMPLE_H_*/
